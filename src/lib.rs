3fd5de20ffe55b034adf6e341dd9186fbb1ec068
Andrei Kalfas <akalfas@adobe.com>
2024-07-03 11:30:02 +0300

    u16 will do just fine

--- src/lib.rs
+++ src/lib.rs
@@ -8,17 +8,17 @@ use wasm_bindgen::prelude::*;
 #[global_allocator]
 static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
 
-fn mean(data: &[u32]) -> u32 {
-    let sum: u32 = data.iter().sum();
-    sum / (data.len() as u32)
+fn mean(data: &[u16]) -> u16 {
+    let sum: u16 = data.iter().sum();
+    sum / (data.len() as u16)
 }
 
-fn variance(data: &[u32], mean: u32) -> u32 {
-    let variance: u32 = data.iter().map(|&value| (value - mean).pow(2)).sum();
-    variance / (data.len() as u32)
+fn variance(data: &[u16], mean: u16) -> u16 {
+    let variance: u16 = data.iter().map(|&value| (value - mean).pow(2)).sum();
+    variance / (data.len() as u16)
 }
 
-fn erf(x: f64) -> f64 {
+fn erf(x: f32) -> f32 {
     // constants
     let a1 = 0.254829592;
     let a2 = -0.284496736;
@@ -36,13 +36,13 @@ fn erf(x: f64) -> f64 {
 }
 
 #[wasm_bindgen]
-pub fn t_test(left: &[u32], right: &[u32]) -> f64 {
+pub fn t_test(left: &[u16], right: &[u16]) -> f32 {
     let mean_left = mean(left);
     let mean_right = mean(right);
     let variance_left = variance(left, mean_left);
     let variance_right = variance(right, mean_right);
     let pooled_variance = (variance_left + variance_right) / 2;
     let t_value = (mean_left - mean_right) as f64 / (pooled_variance as f64 * (1 as f64 / left.len() as f64 + 1 as f64 / right.len() as f64)).sqrt();
-    let p = 1.0 - (0.5 + 0.5 * erf(t_value / 2.0f64.sqrt()));
+    let p = 1.0 - (0.5 + 0.5 * erf((t_value / 1.41) as f32));
     p
 }
